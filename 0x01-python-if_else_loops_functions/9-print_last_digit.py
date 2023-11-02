a96b95f10879cd010eea7320a466d8166a90525d
im2023 <alximamkoutabo@gmail.com>
2023-11-02 03:10:35 +0100

    if else loops

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number < 0:
+        lst_digit = number % -10
+        lst_digit *= -1
+    else:
+        lst_digit = number % 10
+    print("{:d}".format(lst_digit), end="")
+    return lst_digit
