c2d9e7aaf16a3aa218c3b7a0fb74fb34b0b47855
im2023 <alximamkoutabo@gmail.com>
2023-11-09 03:14:07 +0100

    more python data structures

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,7 +2,7 @@
 def complex_delete(a_dictionary, value):
     del_keys = [kadnas for kadnas, vall in a_dictionary.items() if vall == value]
 
-    for kadnas in del_keys:
-        del a_dictionary[kadnas]
+    for key in del_keys:
+        del a_dictionary[key]
 
     return a_dictionary
