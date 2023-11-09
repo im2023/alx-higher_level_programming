a654953779e997e635f2fb08559ef56b61e716b7
im2023 <alximamkoutabo@gmail.com>
2023-11-09 01:42:39 +0100

    more python data structures

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,1 +1,7 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    sort = sorted(a_dictionary.keys())
 
+    for allias in sort:
+        val = a_dictionary[allias]
+        print("{}: {}".format(allias, val))
