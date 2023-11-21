fdc6b7b1a78b27faddbfdfdbc3c210d08e44aea9
im2023 <alximamkoutabo@gmail.com>
2023-11-21 20:05:09 +0100

    python exceptions

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+
+"""Defining class"""
+
+
 class Square:
+    """the Representation of square."""
+
     def __init__(self, size):
+        """Initialization of a new Square.
+        Args:
+            size (int):new square size.
+        """
         self.__size = size
