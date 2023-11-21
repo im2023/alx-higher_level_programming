d097d73ae0210158ba93d73f02706cf68d672e1a
im2023 <alximamkoutabo@gmail.com>
2023-11-21 20:23:48 +0100

    python exceptions

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,8 +8,8 @@ class Square:
 
     def __init__(self, size=0):
         """the Squar class contructor
-        Arguments:
-            size (int):size of new square.
+        Args:
+            size (int): size of new square.
         """
         if size < 0:
             raise ValueError("size must be >= 0")
