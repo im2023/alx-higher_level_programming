1ea454aacb86989f9e356e7ab46b546a78c0715e
im2023 <alximamkoutabo@gmail.com>
2023-11-21 21:52:00 +0100

    python exceptions

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+class Square:
+    """
+    This class represents a square and provides methods to work with it.
+    """
+
+    def __init__(self, size=0):
+        """
+        Creates a new square.
+
+        Args:
+            size (int): The length of each side of the square (default is 0).
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+        Gets the size (length of a side) of the square.
+
+        Returns:
+            int: The size of the square.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Sets the size of the square.
+
+        Args:
+            value (int): The size to set.
+
+        Raises:
+            TypeError: If the provided size is not an integer.
+            ValueError: If the provided size is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """
+        Calculates the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size * self.__size
+
+    def my_print(self):
+        """
+        Prints the square using '#' characters.
+
+        If the size is 0, prints an empty line.
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
