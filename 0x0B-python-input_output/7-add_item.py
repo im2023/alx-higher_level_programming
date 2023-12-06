5bf0e2584ef7547867fc80390169fb903590505b
im2023 <alximamkoutabo@gmail.com>
2023-12-06 03:27:10 +0100

    python input output

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Save the argument to a file"""
+import sys
+
+if __name__ == "__main__":
+    save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+    load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+    try:
+        x = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        x = []
+    x.extend(sys.argv[1:])
+    save_to_json_file(x, "add_item.json")
---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines a class Student."""
+
+
+class Student:
+    """the Student class body"""
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialization of student props
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return self.__dict__
