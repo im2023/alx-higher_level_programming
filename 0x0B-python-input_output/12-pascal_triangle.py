09654451e350970dbec8c09e12a84e5e3817e42c
im2023 <alximamkoutabo@gmail.com>
2023-12-06 03:38:50 +0100

    python input output

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -2,13 +2,13 @@
 """the pascal_traigle module"""
 
 
-def pascal_triangle(x):
+def pascal_triangle(n):
     """pascal traigle body class"""
-    if x <= 0:
+    if n <= 0:
         return []
 
     triangles = [[1]]
-    while len(triangles) != x:
+    while len(triangles) != n:
         trgl = triangles[-1]
         tmp = [1]
         for t in range(len(trgl) - 1):
