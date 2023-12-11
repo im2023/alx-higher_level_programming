04f852cc5db92afa1d8113cab602d99be336bbad
im2023 <alximamkoutabo@gmail.com>
2023-12-12 00:42:51 +0100

    almost a circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,5 +22,5 @@ class Base:
     def to_json_string(list_dictionaries):
         """convert the result to Json."""
         if list_dictionaries is None or not list_dictionaries == []:
-            return []
+            return "[]"
         return json.dumps(list_dictionaries)
