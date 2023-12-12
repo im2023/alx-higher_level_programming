9dee598032c69dbc9850c42ebae8657af5db6464
im2023 <alximamkoutabo@gmail.com>
2023-12-12 01:50:43 +0100

    almost a circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -65,3 +65,37 @@ class Base:
                 return [cls.create(**n) for n in list_dicts]
         except IOError:
             return []
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """Writing CSV of list of objects to a file"""
+        fil = cls.__name__ + ".csv"
+        with open(fil, "w", newline="") as csvfile:
+            if list_objs is None or list_objs == []:
+                csvfile.write("[]")
+            else:
+                if cls.__name__ == "Rectangle":
+                    filenames = ["id", "width", "height", "x", "y"]
+                else:
+                    filenames = ["id", "size", "x", "y"]
+                writer = csv.DictWriter(csvfile, fieldnames=filenames)
+                for obj in list_objs:
+                    writer.writerow(obj.to_dictionary())
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """Return a list of class instantiated from a CSV file."""
+        filename = cls.__name__ + ".csv"
+        try:
+            with open(filename, "r", newline="") as csvfile:
+                if cls.__name__ == "Rectangle":
+                    fieldnames = ["id", "width", "height", "x", "y"]
+                else:
+                    fieldnames = ["id", "size", "x", "y"]
+                list_dicts = csv.DictReader(csvfile, fieldnames=fieldnames)
+                list_dicts = [
+                    dict([k, int(v)] for k, v in d.items()) for d in list_dicts
+                ]
+                return [cls.create(**d) for d in list_dicts]
+        except IOError:
+            return []
