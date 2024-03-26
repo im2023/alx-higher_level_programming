4d3f8d9365dd199822d41d564bac3a4204c5aa66
im2023 <alximamkoutabo@gmail.com>
2024-03-26 23:55:16 +0000

    javascript web scraping

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 const request = require('request');
 const endPoint = 'http://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request.get(endPoint, function (err, response, body) {
+request.get(endPoint, function (err, rsp, body) {
   if (err) {
     throw err;
-  } else if (response.statusCode === 200) {
+  } else if (rsp.statusCode === 200) {
     const characters = JSON.parse(body).characters;
     characters.forEach(character => {
-      request.get(character, function (err, response, body) {
+      request.get(character, function (err, rsp, body) {
         if (err) {
           throw err;
-        } else if (response.statusCode === 200) {
+        } else if (rsp.statusCode === 200) {
           console.log(JSON.parse(body).name);
         }
       });
