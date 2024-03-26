756d0ec8c8417f8307f20ea41c0d033480897475
im2023 <alximamkoutabo@gmail.com>
2024-03-26 23:53:04 +0000

    javascript web scraping

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,21 +1,24 @@
 #!/usr/bin/node
-const request = require('request');
-const url = process.argv[2];
 
-request(url, function (err, data, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    let counter = 0;
-    const films = JSON.parse(body).results;
-    for (let result = 0; result < films.length; result++) {
-      const characters = films[result].characters;
-      for (let j = 0; j < characters.length; j++) {
-        if (characters[j] === 'https://swapi-api.hbtn.io/api/people/18/' || characters[j] === 'http://swapi-api.hbtn.io/api/people/18/') {
-          counter += 1;
-        }
+const request = require('request');
+const starWarsUri = process.argv[2];
+let times = 0;
+
+request(starWarsUri, function (_err, _res, body) {
+  body = JSON.parse(body).results;
+
+  for (let i = 0; i < body.length; ++i) {
+    const characters = body[i].characters;
+
+    for (let j = 0; j < characters.length; ++j) {
+      const character = characters[j];
+      const characterId = character.split('/')[5];
+
+      if (characterId === '18') {
+        times += 1;
       }
     }
-    console.log(counter);
   }
+
+  console.log(times);
 });
